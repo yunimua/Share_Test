e251fadc9d270ea2139ba1bb4b30a277a268f960
yunimua <8401051208+yunimua@users.noreply.github.com>
2021-08-05 02:31:25 +0900

    [jsy][branch] commit test

--- src/baseSettings/PosFrame.java
+++ src/baseSettings/PosFrame.java
@@ -8,6 +8,7 @@ public class PosFrame extends JFrame {
 	
 	public void setDefaultOptions() {
 		setSize(1300,800);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); 
 		setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
 		setResizable(false); 
